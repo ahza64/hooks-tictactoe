a44895e114d728c129616f3b22943bbe0eef9012
ahza64 <justinmichaelpettit@gmail.com>
2021-10-01 13:45:15 -0700

    new game button and conditional styling on a win

--- src/App.js
+++ src/App.js
@@ -6,23 +6,20 @@ import { calculateWinner } from './helpers'
 
 import './styles/root.scss'
 
+const NEW_GAME = [
+  { board: Array(9).fill(null), isXNext : true }
+];
+
 const App = () => {
-  const [history, setHistory] = useState([
-    { board: Array(9).fill(null), isXNext : true }
-  ]);
+  const [history, setHistory] = useState(NEW_GAME);
   const [currentMove, setCurrentMove] = useState(0);
-
   const current = history[currentMove];
-
-  console.log(history);
-
-  const winner = calculateWinner(current.board);
+  const {winner, winningSquares} = calculateWinner(current.board);
 
   const handleSquareClick = (position) => {
     if(current.board[position] || winner) {
       return;
     }
-
     setHistory((prev) => {
       const last = prev[prev.length - 1];
 
@@ -41,12 +38,18 @@ const App = () => {
     setCurrentMove(move);
   }
 
+  const onNewGame = () => {
+    setHistory(NEW_GAME);
+    setCurrentMove(0);
+  }
+
   return (
     <div className='app'>
       <h1>TIC TAC TOE</h1>
-      <StatusMessage winner={winner} current={current}/>
-      <Board board={current.board} handleSquareClick={handleSquareClick}/>
-      <History history={history} moveTo={moveTo} currentMove={currentMove}/>
+      <StatusMessage winner={winner} current={current} />
+      <Board board={current.board} handleSquareClick={handleSquareClick} winningSquares={winningSquares} />
+      <button type='button' onClick={onNewGame}>Start new game</button>
+      <History history={history} moveTo={moveTo} currentMove={currentMove} />
     </div>
   )
 };
